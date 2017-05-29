f7519a8544d5fdbfcfe7e78dbc601b3f4cdaa924
LiuHui <1132787527@qq.com>
2017-05-30 00:29:52 +0800

    session2 practice1

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,5 +1,26 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  var arr=[];
+  for (var i=0;i<collection.length;i++){
+    var time=0;
+    if(arr.length==0){
+      arr.push({key:collection[i],count:1});
+    }
+    else{
+      for(var j=0;j<arr.length;j++){
+        if(collection[i]==arr[j].key){
+          arr[j].count++;
+          break;
+        }
+        else{
+          time++;
+        }
+      }
+      if(time==arr.length){
+        arr.push({key:collection[i],count:1});
+      }
+    }
+  }
+  return arr;
 }
