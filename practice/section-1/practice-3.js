a1661c6809ecac3be6b68a63d0f19d468ace14ef
LiuHui <1132787527@qq.com>
2017-05-29 23:30:46 +0800

    session1 practice3

--- practice/section-1/practice-3.js
+++ practice/section-1/practice-3.js
@@ -1,5 +1,13 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  var arr=new Array;
+  for (var i=0;i<collectionA.length;i++){
+    for(var j=0;j<objectB.value.length;j++){
+      if(objectB.value[j]==collectionA[i]){
+        arr.push(collectionA[i]);
+      }
+    }
+  }
+  return arr;
 }
