09bdb346b83d77d6e9ce2b93b93180474d036a4f
LiuHui <1132787527@qq.com>
2017-05-30 15:19:05 +0800

    session3 practice2

--- practice/section-3/practice-2.js
+++ practice/section-3/practice-2.js
@@ -1,5 +1,19 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  for(var i=0;i<collectionA.length;i++){
+    var num=collectionA[i].count;
+    var time=0;
+    for(var j=0;j<objectB.value.length;j++){
+      if(collectionA[i].key==objectB.value[j]){
+        while(num>=3){
+          time++;
+          num-=3;
+        }
+        collectionA[i].count-=time;
+        break;
+      }
+    }
+  }
+  return collectionA;
 }
